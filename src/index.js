290dd9fa7f7cc82b301f3bd48aa281cc54482b33
Abhishek <145745124+abhishek-z2@users.noreply.github.com>
2024-10-04 22:24:56 +0530

    add complete button toggle

--- src/index.js
+++ src/index.js
@@ -25,8 +25,17 @@
 
         const projects = new Set()
 
-        const todos =[];
+        function getProject(){
+            return projects
+        }
 
+        const todos =[];
+        function addProject(project){
+            if(project){
+                projects.add(project)
+                console.log('project added')
+            }
+        }
         function newTodo(title,description,dueDate,priority,notes,project){
             return createTodo(title,description,dueDate,priority,notes,project)
         }
@@ -40,7 +49,6 @@
         }
         
         function addTodo(title,description,dueDate,priority,notes,project){
-            projects.add(project)
             dueDate=formatDate(dueDate)
             addToArray(newTodo(title,description,dueDate,priority,notes,project))
         }
@@ -64,7 +72,7 @@
         }
 
 
-        return {todos,newTodo,addToArray,filterByProject,addTodo,setComplete,getStatus,getAllProjects}
+        return {todos,newTodo,addToArray,filterByProject,addTodo,setComplete,getStatus,getAllProjects,addProject,getProject}
     })()
 
 
@@ -97,6 +105,23 @@
         const dateInput = document.querySelector('#date');
         const notesInput = document.querySelector('#notes');
         const projectInput = document.querySelector('#select-project');
+        const projectDialog = document.querySelector('#project-dialog')
+        const projectAddButton = document.querySelector('.add-project')
+        const projectSubmitButton = document.querySelector('#project-submit-button')
+        const projectDialogInput = document.querySelector('#project')
+
+        projectAddButton.addEventListener('click',()=>{
+            projectDialog.showModal()
+        })
+
+        projectSubmitButton.addEventListener('click',()=>{
+            todoList.addProject(projectDialogInput.value)
+            console.log(projectDialogInput.value)
+            console.log(todoList.getProject())
+            DOMdisplay.projectDisplay()
+            projectDialog.close()
+        })
+        
         // const completeButton = document.querySelector('.todo-complete')
     
         const addNewTodo = document.querySelector('.add-todo');
@@ -120,6 +145,8 @@
     
             todoList.addTodo(title, description, date, priority, notes, project);
             DOMdisplay.loadTodo()
+            DOMdisplay.projectDisplay()
+
             // Reset input fields
             titleInput.value = '';
             descriptionInput.value = '';
@@ -144,37 +171,40 @@
         const todoContainer = document.querySelector('#todo-container')
         const projectHeader = document.querySelector('#header')
 
-        function makeTodoCard(title_,description_,dueDate_,notes_,priority_,index){
+        function makeTodoCard(todo,index){
             const todoCard = document.createElement('div')
             todoCard.classList.add('todo-card')
             const title = document.createElement('div')
             title.classList.add('title')
-            title.textContent=title_
+            title.textContent=todo.title
             todoCard.appendChild(title)
             const description = document.createElement('div')
             description.classList.add('description')
-            description.textContent=description_
+            description.textContent=todo.description
             todoCard.appendChild(description)
             const dueDate = document.createElement('div')
             dueDate.classList.add('due-date')
-            dueDate.textContent=dueDate_
+            dueDate.textContent=todo.dueDate
             todoCard.appendChild(dueDate)
             const notes = document.createElement('div')
             notes.classList.add('notes')
-            notes.textContent=notes_
+            notes.textContent=todo.notes
             todoCard.appendChild(notes)
             const priority = document.createElement('div')
             priority.classList.add('priority')
-            priority.textContent=priority_
+            priority.textContent=todo.priority
             todoCard.appendChild(priority)
             const completeButton = document.createElement('button')
-            completeButton.classList.add('.todo-complete')
-            completeButton.textContent='complete'
+            completeButton.classList.add('todo-complete')
+            if (todo.isComplete) {
+                completeButton.textContent = 'completed';
+            } else {
+                completeButton.textContent = 'complete';
+            }
             completeButton.dataset.index = index
 
-            completeButton.addEventListener('click',(event)=>{
-                const buttonIndex = event.target.dataset.index
-                todoList.setComplete(todoList.todos[buttonIndex])
+            completeButton.addEventListener('click',()=>{
+                todoList.setComplete(todo)
                 loadTodo()
             })
             todoCard.appendChild(completeButton)
@@ -186,23 +216,35 @@
             console.log('worked',todoList.todos)
 
             todoContainer.innerHTML=''
-            if(project!=null){
+            if(project){
                 const filteredTodoList= todoList.filterByProject(project)
                 filteredTodoList.forEach((todo,index)=>{
-                    const todoCard = makeTodoCard(todo.title,todo.description,todo.dueDate,todo.notes,todo.priority,index)
+                    const todoCard = makeTodoCard(todo,index)
                     todoContainer.appendChild(todoCard)
 
                 })
                 return
             }
             todoList.todos.forEach((todo,index)=>{
-                const todoCard = makeTodoCard(todo.title,todo.description,todo.dueDate,todo.notes,todo.priority,index)
+                const todoCard = makeTodoCard(todo,index)
                 todoContainer.appendChild(todoCard)
             })
 
 
         }
-        return {loadTodo}
+        const projectList = document.querySelector('.project-list')
+
+        function projectDisplay(){
+            projectList.innerHTML=''
+            const projects = Array.from(todoList.getProject())
+            console.log(projects)
+            projects.forEach((project)=>{
+                const projectItem = document.createElement('li')
+                projectItem.textContent=project
+                projectList.appendChild(projectItem)
+            })
+        }
+        return {loadTodo,projectDisplay}
     })()
 
     // console.log(todoList.getAllProjects())
